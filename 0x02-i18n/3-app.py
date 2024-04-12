171f1e2f6ccbdf36cce84aa744a6d68571870da9
douniabendib <bendib.dounia@gmail.com>
2024-04-12 15:03:29 -0700

    task3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -29,7 +29,7 @@ def get_locale():
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """handle route"""
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == "__main__":
